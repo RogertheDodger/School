681dd40d1320f5ae875378fec57857fe0994d512
RogertheDodger <31708672+RogertheDodger@users.noreply.github.com>
2017-10-17 00:28:10 -0500

    Update .Lab4_PCRomanNum.cpp

--- .Lab4_PCRomanNum.cpp
+++ .Lab4_PCRomanNum.cpp
@@ -10,27 +10,38 @@ int main()
 
     switch( Number )
     {
-        case 1:cout << "I" << endl;
+        case 1:
+            cout << "I" << endl;
             break;
-        case 2:cout << "II" << endl;
+        case 2:
+            cout << "II" << endl;
             break;
-        case 3:cout << "III" << endl;
+        case 3:
+            cout << "III" << endl;
             break;
-        case 4:cout << "IV" << endl;
+        case 4:
+            cout << "IV" << endl;
             break;
-        case 5:cout << "V" << endl;
+        case 5:
+            cout << "V" << endl;
             break;
-        case 6:cout << "VI" << endl;
+        case 6:
+            cout << "VI" << endl;
             break;
-        case 7:cout << "VII" << endl;
+        case 7:
+            cout << "VII" << endl;
             break;
-        case 8:cout << "VIII" << endl;
+        case 8:
+            cout << "VIII" << endl;
             break;
-        case 9:cout << "IX" << endl;
+        case 9:
+            cout << "IX" << endl;
             break;
-        case 10:cout << "X" << endl;
+        case 10:
+            cout << "X" << endl;
             break;
-        default:cout << "Please enter a number 1 through 10." << endl;
+        default:
+            cout << "Please enter a number 1 through 10." << endl;
     }
 
     return 0;
